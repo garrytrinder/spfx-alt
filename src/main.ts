eb9c934cad77e8239059ebaeb0373a69e2f39620
Garry Trinder <garry.trinder@live.com>
2017-02-23 16:38:44 +0000

    Various TSLint fixes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { sayHello } from './greet';
+import { sayHello } from "./greet";
 
-import pnp from 'sp-pnp-js';
+import pnp from "sp-pnp-js";
 
-console.info('ps-site-dev');
+console.log("ps-site-dev");
 
 console.log(sayHello("Typescript!"));
 
-// The @types/sharepoint typings allow us to get intellisense at dev time
+// the @types/sharepoint typings allow us to get intellisense at dev time
 console.log(_spPageContextInfo.webAbsoluteUrl);
 
-// Uses the PnP-Core-JS library to get the current web and return the Title
+// uses the PnP-Core-JS library to get the current web and return the Title
 pnp.sp.web.get().then(w => { console.log(w.Title); });
